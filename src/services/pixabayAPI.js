c713d849d208ca05a07c7cb6aca1d5d8dea143be
Ivan Fesenko <pharmivan@gmail.com>
2020-12-02 19:38:16 +0200

    before refactor

--- src/services/pixabayAPI.js
+++ src/services/pixabayAPI.js
@@ -4,12 +4,7 @@ const KEY = 'key=18267918-a545f4b922b3d8b59313b99e1';
 const BASEURL =
   'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=';
 
-// const url = `${baseURL}${searchQuery}&page=${page}&per_page=12&${key}`;
-
-// const forTest =
-//   'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=fire&page=1&per_page=12&key=18267918-a545f4b922b3d8b59313b99e1';
-
-async function getData(query = 'fire', page = '1') {
+async function getData(query, page) {
   try {
     const response = await Axios.get(
       `${BASEURL}${query}&page=${page}&per_page=12&${KEY}`,
